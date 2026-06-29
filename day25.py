ff5a3d637940fd5992322f4e582fdf58f47676ae
Nighthawler001 <fbinito2004@gmail.com>
2026-06-29 11:29:50 +0530

    day25 python enumurate practiced file

---
+++ day25.py
@@ -0,0 +1,9 @@
+# games = ["Ark", "Minecraft", "Valorant"]
+
+# for i, game in enumerate(games, 5):
+#     print(i, game)
+
+games = ["Ark", "Minecraft", "Valorant"]
+
+for i in range(len(games)):
+    print(i, games[i])
