69775167ba2aa2c0ef373d496c64d05951732ab2
Nighthawler001 <fbinito2004@gmail.com>
2026-06-30 12:05:48 +0530

    Day 28 python sorted practiced file added

---
+++ day28.py
@@ -0,0 +1,13 @@
+# games = ["Valorant", "Ark", "Minecraft"]
+
+# sorted_games = sorted(games)
+
+# print("-"*30)
+# print(sorted_games)
+# print(games)
+# print("-"*30)
+# games.sort()
+# print(games)
+# print("-"*30)
+game ="minecraft"
+print(lambda game:len(game))
