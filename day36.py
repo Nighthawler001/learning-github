5d1a786b4f0659178ee2cd0bbf90584443aa023d
Nighthawler001 <fbinito2004@gmail.com>
2026-07-05 11:38:32 +0530

    Day 36 modified

--- day36.py
+++ day36.py
@@ -14,18 +14,22 @@
 # Steam Library - Ark - Played hours: 220
 # Steam Library - Minecraft - Played hours: 120
 
-class Student():
+# class Student():
 
-    school = "Global Gaming School"
+#     school = "Global Gaming School"
 
-    def __init__(self,name,grade):
-        self.name = name
-        self.grade = grade 
+#     def __init__(self,name,grade):
+#         self.name = name
+#         self.grade = grade 
 
-student1 = Student("Night","A")
-student2 = Student("Alex","E")
-student3 = Student("John","F")
+# student1 = Student("Night","A")
+# student2 = Student("Alex","E")
+# student3 = Student("John","F")
 
-print(f"{student1.name} from {student1.school} Scored {student1.grade} grade")
-print(f"{student2.name} from {student2.school} Scored {student2.grade} grade")
-print(f"{student3.name} from {student3.school} Scored {student3.grade} grade")
+# print(f"{student1.name} from {student1.school} Scored {student1.grade} grade")
+# print(f"{student2.name} from {student2.school} Scored {student2.grade} grade")
+# print(f"{student3.name} from {student3.school} Scored {student3.grade} grade")
+# O/P 
+# Night from Global Gaming School Scored A grade
+# Alex from Global Gaming School Scored E grade
+# John from Global Gaming School Scored F grade
