391f3cb9fe373ad164b1258c1c04771c470a49a4
Nighthawler001 <fbinito2004@gmail.com>
2026-07-05 11:37:11 +0530

    Day 36 python class and instance variables practiced file added

---
+++ day36.py
@@ -0,0 +1,31 @@
+# class Game():
+#     company = "Steam"
+
+#     def __init__(self,title,hours):
+#         self.title = title
+#         self.hours = hours
+    
+# game1 = Game("Ark", 220)
+# game2 = Game("Minecraft", 120)
+
+# print(f"Steam Library - {game1.title} - Played hours: {game1.hours}")
+# print(f"Steam Library - {game2.title} - Played hours: {game2.hours}")
+# O/P 
+# Steam Library - Ark - Played hours: 220
+# Steam Library - Minecraft - Played hours: 120
+
+class Student():
+
+    school = "Global Gaming School"
+
+    def __init__(self,name,grade):
+        self.name = name
+        self.grade = grade 
+
+student1 = Student("Night","A")
+student2 = Student("Alex","E")
+student3 = Student("John","F")
+
+print(f"{student1.name} from {student1.school} Scored {student1.grade} grade")
+print(f"{student2.name} from {student2.school} Scored {student2.grade} grade")
+print(f"{student3.name} from {student3.school} Scored {student3.grade} grade")
