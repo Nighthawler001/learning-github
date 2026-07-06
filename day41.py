e10194aeebee92f4bb536750d32d00067eb5d31c
Nighthawler001 <fbinito2004@gmail.com>
2026-07-06 12:27:56 +0530

    day 41 __repr__

---
+++ day41.py
@@ -0,0 +1,28 @@
+# class Student():
+#     def __init__(self,name):
+#         self.name = name
+    
+#     def __repr__(self):
+#         return f"Student('{self.name}')"
+
+# student = Student("Night")
+
+# print(student)
+# O/P 
+# Student('Night')
+
+class Book():
+    def __init__(self,title,author):
+        self.title = title
+        self.author = author
+    
+    def __repr__(self):
+        return f"Book('{self.title}','{self.author}')"
+
+books = [
+    Book("Atomic Habits", "James Clear"),
+    Book("Clean Code", "Robert C. Martin")
+]
+
+print(books)
+    
